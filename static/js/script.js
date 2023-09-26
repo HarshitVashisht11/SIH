a58686bee635fe5449e957a2c9d3d5f07d61878a
HarshitVashisht11 <harshitvashisht07@gmail.com>
2023-09-26 14:31:31 +0530

    Updated Code

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,24 @@
 function videoconAnimation() {
-    var videocon = document.querySelector("#right");
+    var videocon = document.querySelector("#content");
     var playbtn = document.querySelector("#play");
-
     videocon.addEventListener("mouseenter", function () {
-        gsap.to(playbtn, {
-            scale: 1,
-            opacity: 1,
-        });
+      gsap.to(playbtn, {
+        scale: 1,
+        opacity: 1,
+      });
     });
-
     videocon.addEventListener("mouseleave", function () {
-        gsap.to(playbtn, {
-            scale: 0,
-            opacity: 0,
-        });
+      gsap.to(playbtn, {
+        scale: 0,
+        opacity: 0,
+      });
     });
-
-    videocon.addEventListener("mousemove", function (e) {
-        gsap.to(playbtn, {
-            left: e.clientX - 70 + "px",
-            top: e.clientY - 80 + "px",
-        });
+    document.addEventListener("mousemove", function (dets) {
+      gsap.to(playbtn, {
+        left: dets.x - 70,
+        top: dets.y - 80,
+      });
     });
-}
+  }
+  videoconAnimation();
 
-videoconAnimation();
